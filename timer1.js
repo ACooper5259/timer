c627097eebba347df1b9eae7543ee55812fc8a70
Audrey Cooper <audrey_cooper@me.com>
2020-08-25 15:54:38 -0700

    lint timer1

--- timer1.js
+++ timer1.js
@@ -2,7 +2,7 @@ const args = process.argv;
 const beepDelays = args.slice(2);
 
 const howManyBeeps = function (beepsDelays) {
-  for (x = beepDelays.length; x >= 0; x--) {
+  for (let x = beepDelays.length; x >= 0; x--) {
     beepDelays[x] = Number(beepDelays[x]);
     //console.log(beepDelays);
 
@@ -15,7 +15,7 @@ const howManyBeeps = function (beepsDelays) {
 
 const timer = function (numOfBeeps) {
   numOfBeeps = howManyBeeps(beepDelays);
-  for (beep of numOfBeeps) {
+  for (let beep of numOfBeeps) {
     beep *= 1000;
     setTimeout(() => {
       process.stdout.write(".");
